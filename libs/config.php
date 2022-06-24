a2db789b38b047c3aa3c6937c1f0b2132a196fbe
yukio <funky.kotetsu@gmail.com>
2022-06-24 23:55:46 +0900

    add error message & valid value

--- libs/config.php
+++ libs/config.php
@@ -1,4 +1,18 @@
 <?php
     //title
     define('APP_NAME', 'My Calendar');
+
+    //color
+    $color_list = [
+
+      "bg-danger" => "赤",
+      "bg-warning" => "オレンジ",
+      "bg-primary" => "青",
+      "bg-info" => "水色",
+      "bg-success" => "緑",
+      "bg-dark" => "黒",
+      "bg-secondary" => "グレー"
+
+    ];
+    define('COLOR_LIST', $color_list);
 ?>
